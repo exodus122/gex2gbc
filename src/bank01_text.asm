71300540cd6d40406800352bf39191b2046b2e79
Exodus <7614885+exodus122@users.noreply.github.com>
2025-08-30 03:03:11 -0400

    Split up menu bank

---
+++ src/bank01_text.asm
@@ -0,0 +1,478 @@
+data_01_5ccf:
+    db   "START", TextTerminator
+data_01_5cd5:
+    db   "SOUND", TextTerminator
+data_01_5cdb:
+    db   "PASSWORD", TextTerminator
+data_01_5ce4:
+    db   "PAUSED", TextTerminator
+data_01_5ceb:
+    db   "RESUME", TextTerminator
+data_01_5cf2:
+    db   "QUIT", TextTerminator
+data_01_5cf7:
+    db   "EXIT", TextTerminator
+data_01_5cfc:
+    db   "QUIT GAME", TextTerminator
+data_01_5d06:
+    db   "EXIT TO MAP", TextTerminator
+data_01_5d12:
+    db   "NO WAY!", TextTerminator
+data_01_5d1a:
+    db   "OKAY", TextTerminator
+    db   "TIME UP!", TextTerminator
+data_01_5d28:
+    db   "GAME OVER", TextTerminator
+data_01_5d32:
+    db   "RESUME PLAY", TextTerminator
+data_01_5d3e:
+    db   "SEE PASSWORD", TextTerminator
+
+data_01_5d4b:
+    db   "GAME STATS", TextTerminator
+data_01_5d56:
+    db   "ENTERING...", TextTerminator
+data_01_5d62:
+    db   "X", TextTerminator
+    db   "CONGRATULATIONS!", TextTerminator
+    db   "REWARD", TextTerminator
+    db   "HIDDEN", TextTerminator
+    db   "PRESS B TO CONTINUE", TextTerminator
+    db   "0 OF 3 RED REMOTES FOUND", TextTerminator
+    db   "1 OF 3 RED REMOTES FOUND", TextTerminator
+    db   "2 OF 3 RED REMOTES FOUND", TextTerminator
+    db   "3 OF 3 RED REMOTES FOUND", TextTerminator
+    db   "0 OF 2 RED REMOTES FOUND", TextTerminator
+    db   "1 OF 2 RED REMOTES FOUND", TextTerminator
+    db   "2 OF 2 RED REMOTES FOUND", TextTerminator
+    db   "0 OF 1 RED REMOTES FOUND", TextTerminator
+    db   "1 OF 1 RED REMOTES FOUND", TextTerminator
+    db   "0 OF 1 GOLD REMOTES FOUND", TextTerminator
+    db   "1 OF 1 GOLD REMOTES FOUND", TextTerminator
+    db   "CHOOSE A HINT THEN PRESS B TO CONTINUE", TextTerminator
+    db   "PRESS B TO CONTINUE", TextTerminator
+
+data_01_5ee7:
+    dw   data_01_5efd
+    dw   data_01_5f08
+    dw   data_01_5f18
+    dw   data_01_5f28
+    dw   data_01_5f38
+    dw   data_01_5f42
+    dw   data_01_5f51
+    dw   data_01_5f5b
+    dw   data_01_5f63
+    dw   data_01_5f71
+    dw   data_01_5f80
+
+data_01_5efd:
+    db   "GAME STATS", TextTerminator
+data_01_5f08:
+    db   "CIRCUIT CENTRAL", TextTerminator
+data_01_5f18:
+    db   "KUNG-FU THEATRE", TextTerminator
+data_01_5f28:
+    db   "PREHIST CHANNEL", TextTerminator
+data_01_5f38:
+    db   "REZOPOLIS", TextTerminator
+data_01_5f42:
+    db   "ROCKET CHANNEL", TextTerminator
+data_01_5f51:
+    db   "SCREAM TV", TextTerminator
+data_01_5f5b:
+    db   "TOON TV", TextTerminator
+data_01_5f63:
+    db   "BONUS BONANZA", TextTerminator
+data_01_5f71:
+    db   "SECRET STATION", TextTerminator
+data_01_5f80:
+    db   "BOSS TV", TextTerminator
+
+data_01_5f88:
+    dw   .data_01_5f90
+    dw   .data_01_5fa4
+    dw   .data_01_5fa5
+    dw   .data_01_5fa6
+
+.data_01_5f90:
+    db   "THE MEDIA DIMENSION", TextTerminator
+.data_01_5fa4:
+    db   TextTerminator
+.data_01_5fa5:
+    db   TextTerminator
+.data_01_5fa6:
+    db   TextTerminator
+
+data_01_5fa7:
+    dw   .data_01_5faf
+    dw   .data_01_5fbb
+    dw   .data_01_5fd6
+    dw   .data_01_5feb
+
+.data_01_5faf:
+    db   "OUT OF TOON", TextTerminator
+.data_01_5fbb:
+    db   "JUMP TO THE TEETERING ROCK", TextTerminator
+.data_01_5fd6:
+    db   "HUNT THE TWO HUNTERS", TextTerminator
+.data_01_5feb:
+    db   "WHACK FIVE PURPLE MUSHROOMS", TextTerminator
+
+data_01_6007:
+    dw   .data_01_600f
+    dw   .data_01_601b
+    dw   .data_01_6037
+    dw   .data_01_6050
+
+.data_01_600f:
+    db   "SMELLRAISER", TextTerminator
+.data_01_601b:
+    db   "SURVIVE THE HAUNTED MANSION", TextTerminator
+.data_01_6037:
+    db   "SMASH FIVE BLOOD COOLERS", TextTerminator
+.data_01_6050:
+    db   "RIDE THE HAUNTED ELEVATOR", TextTerminator
+
+data_01_606a:
+    dw   .data_01_6072
+    dw   .data_01_6083
+    dw   .data_01_6098
+    dw   .data_01_60ab
+
+.data_01_6072:
+    db   "FRANKENSTEINFELD", TextTerminator
+.data_01_6083:
+    db   "RUN THE AXE GAUNTLET", TextTerminator
+.data_01_6098:
+    db   "HEAD DOWN THE RAMP", TextTerminator
+.data_01_60ab:
+    db   "STICK ACROSS THE TOWER OF DOOM", TextTerminator
+
+data_01_60ca:
+    dw   .data_01_60d2
+    dw   .data_01_60e1
+    dw   .data_01_60ff
+    dw   .data_01_611a
+
+.data_01_60d2:
+    db   "WWW.DOTCOM.COM", TextTerminator
+.data_01_60e1:
+    db   "SCALE THE BIONIC LAUNCH TOWER", TextTerminator
+.data_01_60ff:
+    db   "CROSS THE DATA BUS BRIDGES", TextTerminator
+.data_01_611a:
+    db   TextTerminator
+
+data_01_611b:
+    dw   .data_01_6123
+    dw   .data_01_6132
+    dw   .data_01_614b
+    dw   .data_01_615e
+    
+.data_01_6123:
+    db   "MAO TSE TONGUE", TextTerminator
+.data_01_6132:
+    db   "DEFEAT THE DEADLY DRAGON", TextTerminator
+.data_01_614b:
+    db   "TRAVERSE THE STEPS", TextTerminator
+.data_01_615e:
+    db   TextTerminator
+
+data_01_615f:
+    dw   .data_01_6167, .data_01_6168, .data_01_6169, .data_01_616a
+
+.data_01_6167:
+    db   TextTerminator
+.data_01_6168:
+    db   TextTerminator
+.data_01_6169:
+    db   TextTerminator
+.data_01_616a:
+    db   TextTerminator
+
+data_01_616b:
+    dw   .data_01_6173, .data_01_6181, .data_01_6199, .data_01_61ab
+.data_01_6173:
+    db   "PANGAEA 90210", TextTerminator
+.data_01_6181:
+    db   "ASSAULT THE LAVA ISLAND", TextTerminator
+.data_01_6199:
+    db   "CLIMB THE VOLCANO", TextTerminator
+.data_01_61ab:
+    db   TextTerminator
+
+data_01_61ac:    
+    dw   .data_01_61b4, .data_01_61c1, .data_01_61d0, .data_01_61e1
+    
+.data_01_61b4:
+    db   "FINE TOONING", TextTerminator
+.data_01_61c1:
+    db   "CLIMB THE TREE", TextTerminator
+.data_01_61d0:
+    db   "STORM THE CASTLE", TextTerminator
+.data_01_61e1:
+    db   TextTerminator
+
+data_01_61e2:    
+    dw   .data_01_61ea, .data_01_61f8, .data_01_620f, .data_01_6224
+    
+.data_01_61ea:
+    db   "THIS OLD CAVE", TextTerminator
+.data_01_61f8:
+    db   "WATCH FOR FALLING LAVA", TextTerminator
+.data_01_620f:
+    db   "RIDE THE STEAM VENTS", TextTerminator
+.data_01_6224:
+    db   "BOUNCE UP OVER THE CHASM", TextTerminator
+
+data_01_623d:    
+    dw   .data_01_6245, .data_01_625e, .data_01_6270, .data_01_6283
+    
+.data_01_6245:
+    db   "HONEY I SHRUNK THE GECKO", TextTerminator
+.data_01_625e:
+    db   "CHARGE TO THE TOP", TextTerminator
+.data_01_6270:
+    db   "FIND THE I.O TOWER", TextTerminator
+.data_01_6283:
+    db   "CHARGE THE A.C.T. STEPS", TextTerminator
+
+data_01_629b:    
+    dw   .data_01_62a3, .data_01_62ad, .data_01_62c4, .data_01_62e0
+    
+.data_01_62a3:
+    db   "POLTERGEX", TextTerminator
+.data_01_62ad:
+    db   "ASCEND THE GHOST TOWER", TextTerminator
+.data_01_62c4:
+    db   "REACH THE TOP OF THE MORGUE", TextTerminator
+.data_01_62e0:
+    db   "SMASH EIGHT BLOOD COOLERS", TextTerminator
+
+data_01_62fa:    
+    dw   .data_01_6302, .data_01_6303, .data_01_6304, .data_01_6305
+    
+.data_01_6302:
+    db   TextTerminator
+.data_01_6303:
+    db   TextTerminator
+.data_01_6304:
+    db   TextTerminator
+.data_01_6305:
+    db   TextTerminator
+
+data_01_6306:    
+    dw   .data_01_630e, .data_01_6322, .data_01_633c, .data_01_6358
+    
+.data_01_630e:
+    db   "SAMURAI NIGHT FEVER", TextTerminator
+.data_01_6322:
+    db   "NAVIGATE THE POWER TOWERS", TextTerminator
+.data_01_633c:
+    db   "RIDE THE FLOATING PLATFORMS", TextTerminator
+.data_01_6358:
+    db   "CLIMB THE TOWERING TEMPLE", TextTerminator
+
+data_01_6372:    
+    dw   .data_01_637a, .data_01_6394, .data_01_63b2, .data_01_63b3
+    
+.data_01_637a:
+    db   "NO WEDDINGS AND A FUNERAL", TextTerminator
+.data_01_6394:
+    db   "PENETRATE REZ'S INNER SANCTUM", TextTerminator
+.data_01_63b2:
+    db   TextTerminator
+.data_01_63b3:
+    db   TextTerminator
+
+data_01_63b4:    
+    dw   .data_01_63bc, .data_01_63bd, .data_01_63be, .data_01_63bf
+
+.data_01_63bc:    
+    db   TextTerminator
+.data_01_63bd:
+    db   TextTerminator
+.data_01_63be:
+    db   TextTerminator
+.data_01_63bf:
+    db   TextTerminator
+
+data_01_63c0:    
+    dw   .data_01_63c8, .data_01_63da, .data_01_63fb, .data_01_63fc
+
+.data_01_63c8:
+    db   "THURSDAY THE 12TH", TextTerminator
+.data_01_63da:
+    db   "FIND THE ITEMS IN THE GIVEN TIME", TextTerminator
+.data_01_63fb:
+    db   TextTerminator
+.data_01_63fc:
+    db   TextTerminator
+
+data_01_63fd:    
+    dw   .data_01_6405, .data_01_6406, .data_01_6407, .data_01_6408
+    
+.data_01_6405:
+    db   TextTerminator
+.data_01_6406:
+    db   TextTerminator
+.data_01_6407:
+    db   TextTerminator
+.data_01_6408:
+    db   TextTerminator
+
+data_01_6409:    
+    dw   .data_01_6411, .data_01_6412, .data_01_6413, .data_01_6414
+    
+.data_01_6411:
+    db   TextTerminator
+.data_01_6412:
+    db   TextTerminator
+.data_01_6413:
+    db   TextTerminator
+.data_01_6414:
+    db   TextTerminator
+
+data_01_6415:    
+    dw   .data_01_641d, .data_01_641e, .data_01_641f, .data_01_6420
+    
+.data_01_641d:
+    db   TextTerminator
+.data_01_641e:
+    db   TextTerminator
+.data_01_641f:
+    db   TextTerminator
+.data_01_6420:
+    db   TextTerminator
+
+data_01_6421:    
+    dw   .data_01_6429, .data_01_642a, .data_01_642b, .data_01_642c
+    
+.data_01_6429:
+    db   TextTerminator
+.data_01_642a:
+    db   TextTerminator
+.data_01_642b:
+    db   TextTerminator
+.data_01_642c:
+    db   TextTerminator
+
+data_01_642d:    
+    dw   .data_01_6435, .data_01_644c, .data_01_646d, .data_01_646e
+    
+.data_01_6435:
+    db   "LIZARD IN A CHINA SHOP", TextTerminator
+.data_01_644c:
+    db   "FIND THE ITEMS IN THE GIVEN TIME", TextTerminator
+.data_01_646d:
+    db   TextTerminator
+.data_01_646e:
+    db   TextTerminator
+
+data_01_646f:    
+    dw   .data_01_6477, .data_01_6482, .data_01_64a3, .data_01_64a4
+    
+.data_01_6477:
+    db   "BUGGED OUT", TextTerminator
+.data_01_6482:
+    db   "FIND THE ITEMS IN THE GIVEN TIME", TextTerminator
+.data_01_64a3:
+    db   TextTerminator
+.data_01_64a4:
+    db   TextTerminator
+
+data_01_64a5:    
+    dw   .data_01_64ad, .data_01_64bc, .data_01_64dd, .data_01_64de
+    
+.data_01_64ad:
+    db   "CHIPS AND DIPS", TextTerminator
+.data_01_64bc:
+    db   "FIND THE ITEMS IN THE GIVEN TIME", TextTerminator
+.data_01_64dd:
+    db   TextTerminator
+.data_01_64de:
+    db   TextTerminator
+
+data_01_64df:    
+    dw   .data_01_64e7, .data_01_64f5, .data_01_6510, .data_01_6511
+    
+.data_01_64e7:
+    db   "LAVA DABA DOO", TextTerminator
+.data_01_64f5:
+    db   "NAVIGATE THE RIVER OF FIRE", TextTerminator
+.data_01_6510:
+    db   TextTerminator
+.data_01_6511:
+    db   TextTerminator
+
+data_01_6512:    
+    dw   .data_01_651a, .data_01_6532, .data_01_654e, .data_01_654f
+    
+.data_01_651a:
+    db   "TEXAS CHAINSAW MANICURE", TextTerminator
+.data_01_6532:
+    db   "RIDE THE FLOATING FURNITURE", TextTerminator
+.data_01_654e:
+    db   TextTerminator
+.data_01_654f:
+    db   TextTerminator
+
+data_01_6550:    
+    dw   .data_01_6558, .data_01_656b, .data_01_6580, .data_01_65a6
+    
+.data_01_6558:
+    db   "MAZED AND CONFUSED", TextTerminator
+.data_01_656b:
+    db   "PASS THE T.V. FOREST", TextTerminator
+.data_01_6580:
+    db   "CROSS THE BLUE BEAMS TO THE REZ TOWER", TextTerminator
+.data_01_65a6:
+    db   TextTerminator
+
+data_01_65a7:    
+    dw   .data_01_65af, .data_01_65b0, .data_01_65b1, .data_01_65b2
+    
+.data_01_65af:
+    db   TextTerminator
+.data_01_65b0:
+    db   TextTerminator
+.data_01_65b1:
+    db   TextTerminator
+.data_01_65b2:
+    db   TextTerminator
+
+data_01_65b3:    
+    dw   .data_01_65bb, .data_01_65bc, .data_01_65bd, .data_01_65be
+    
+.data_01_65bb:
+    db   TextTerminator
+.data_01_65bc:
+    db   TextTerminator
+.data_01_65bd:
+    db   TextTerminator
+.data_01_65be:
+    db   TextTerminator
+
+data_01_65bf:    
+    dw   .data_01_65c7, .data_01_65c8, .data_01_65c9, .data_01_65ca
+    
+.data_01_65c7:
+    db   TextTerminator
+.data_01_65c8:
+    db   TextTerminator
+.data_01_65c9:
+    db   TextTerminator
+.data_01_65ca:
+    db   TextTerminator
+
+data_01_65cb:    
+    dw   .data_01_65d3, .data_01_65dd, .data_01_65fc, .data_01_65fd,
+    
+.data_01_65d3:
+    db   "CHANNEL Z", TextTerminator
+.data_01_65dd:
+    db   "DEFEAT REZ IN THE FINAL BATTLE", TextTerminator
+.data_01_65fc:
+    db   TextTerminator
+.data_01_65fd:
+    db   TextTerminator
